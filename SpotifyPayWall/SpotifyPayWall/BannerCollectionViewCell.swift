aa8225e5492fb49fe4c0599bf1db7e18510ac26d
KayAhn0126 <dkseotn123@gmail.com>
2022-08-29 22:34:17 +0900

    feat: CollectionView Cell을 관리할 파일 추가

---
+++ SpotifyPayWall/SpotifyPayWall/BannerCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BannerCollectionViewCell.swift
+//  SpotifyPayWall
+//
+//  Created by Kay on 2022/08/29.
+//
+
+import UIKit
+
+class BannerCollectionViewCell: UICollectionViewCell {
+    
+}
